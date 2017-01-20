117f4ecc3cfb9241fc76ddffe446863b9f15e8b4
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-01-20 09:21:21 -0800

    Adding a couple more E2E tests

--- test-e2e/e2e.test.js
+++ test-e2e/e2e.test.js
@@ -5,4 +5,22 @@ describe('BigToDo App', () => {
     expect(browser.getTitle()).toEqual('The BigToDo');
   });
 
+  describe('initial visit to page', () => {
+
+    it ('asks for signin/signup', () => {
+      browser.get('/');
+      expect(element.all(by.css('user-auth signin h2')).first().getText()).toEqual('Sign in to access your account');
+    });
+
+    it ('defaults to signin', () => {
+      expect(element.)
+    });
+
+    it ('lands on the welcome page', () => {
+      browser.get('/');
+      expect(element.all(by.css('welcome h2')).first().getText()).toEqual('Welcome to the BigToDo App!');
+    });
+
+  });  
+
 });
