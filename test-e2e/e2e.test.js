437e18f6457fa46629a3e52b95dd84c71de6fb57
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-01-31 10:36:39 -0800

    Added userService unit testing

--- test-e2e/e2e.test.js
+++ test-e2e/e2e.test.js
@@ -1,4 +1,23 @@
+const request = require('request');
+const token = require('../src/services/token-service');
+
 describe('BigToDo App', () => {
+
+  // TODO: Set up to use the 'test' database
+
+  // TODO: Drop the db / collections before running tests
+
+  // TODO: Provide test user info
+  beforeAll (() => {
+    const options = {
+      baseUrl: 'http://localhost:3000',
+      url: '/api/auth/signin',
+      method: 'POST'
+    };
+
+    let myToken = token.get();
+    console.log('myToken ', myToken);
+  });
 
   it ('has a title', () => {
     browser.get('/');
@@ -13,12 +32,42 @@ describe('BigToDo App', () => {
     });
 
     it ('defaults to signin', () => {
-      expect(element.)
+      expect(element.all(by.css('input[ng-model="$ctrl.action"]:checked')).first().getAttribute('value')).toEqual('signin');
     });
 
-    it ('lands on the welcome page', () => {
+    it ('signs up test user and goes to welcome page', () => {
+      element.all(by.css('input[ng-model="$ctrl.action"]')).get(1).click();
+      expect(element.all(by.css('input[ng-model="$ctrl.action"]:checked')).first().getAttribute('value')).toEqual('signup');
+      element(by.id('email')).sendKeys('tester@testing.com');
+      element(by.id('username')).sendKeys('tester');
+      element(by.id('password')).sendKeys('testerpass');
+      element(by.id('signup')).click();
+      expect(element.all(by.css('welcome h2')).first().getText()).toEqual('Welcome to the BigToDo App!');
+    });
+
+    // After authentication the user lands on the welcome page
+
+    xit ('lands on the welcome page', () => {
       browser.get('/');
       expect(element.all(by.css('welcome h2')).first().getText()).toEqual('Welcome to the BigToDo App!');
+    });
+
+    // TODO: Test that user can't sign in unless she's previously signed up
+
+    // TODO: Test that user can't sign up with existing username ("username already exists")
+
+    // TODO: User deletes herself from the database before finishing
+
+    afterAll ((done) => {
+      request
+        .delete('http://localhost:3000/api/users/me')
+        .then((deleted) => {
+          expect(deleted.username).to.equal('tester');
+          done();
+        })
+        .catch(err => {
+          done(err);
+        });
     });
 
   });  
